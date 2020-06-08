94f25a7174964f5d535cf6a7bf0940e8da4c7faa
Haron <haron.silva@gmail.com>
2020-06-08 22:33:34 +0200

    Mais coisas

--- jediAcademy/src/app/app.component.ts
+++ jediAcademy/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import{Student} from "./student/student.model";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,7 +13,9 @@ export class AppComponent {
    * 2. Habilitar o componente que será usado para ter um input com o objeto anteriormente criado.
    * 3. Criar um objeto no modelo esperado e usar via binding no componente html. 
    */
-  luke = {"name":"Luke","isJedi":true,"temple":"Concuscan"}
-  leia = {"name":"Leia","isJedi":false}
-  han ={"name":"Han Solo","isJedi":false}
+  students:Student[] = [
+    {"name":"Luke","isJedi":true,"temple":"Concuscan"},
+    {"name":"Leia","isJedi":true, "temple":"Zé Walter"},
+    {"name":"Han Solo","isJedi":false}
+  ]
 }
